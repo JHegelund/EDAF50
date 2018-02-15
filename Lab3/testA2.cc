2b66e24112d5f0cdfd9d32bfb73a453ba1242ba9
Tesla <wilhelm.lundstroms@gmail.com>
2018-02-15 15:32:32 +0100

    minor fixes

--- Lab3/testA2.cc
+++ Lab3/testA2.cc
@@ -11,7 +11,10 @@ int main(){
 	getline(fin, CPC);
 	primenbr pbn;	
 	assert(CPC == pbn.sievePrime(35));
-	
+	std::ifstream finn("primenbr_sequence35.txt");
+	std::string primenbrs;
+	getline(finn, primenbrs);
+	assert(primenbrs == pbn.primes(pbn.sievePrime(35)));
 	std::cout << "test passed successfully" << std::endl;
 	return 0;
 }
