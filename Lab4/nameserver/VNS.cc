ae60fa34f14ed42bab15339e8837c12f69f0adcc
Tesla <wilhelm.lundstroms@gmail.com>
2018-02-15 17:44:53 +0100

    skeleton structure for VNS.cc

--- Lab4/nameserver/VNS.cc
+++ Lab4/nameserver/VNS.cc
@@ -1,1 +1,19 @@
-#include "nameserverinterface.h"
+#include "VNS.h"
+#include <string>
+
+
+VNS::VNS(){
+
+}
+
+void VNS::insert(const HostName&, const IPAddress&){
+
+}
+
+bool VNS::remove(const HostName&){
+	return false;
+}
+
+IPAddress VNS::lookup(const HostName&) const{
+	return NULL;
+}
